19ad178ae1d43c2781cfdc8db8d320303ace4f82
NKM6795 <31067906+NKM6795@users.noreply.github.com>
2018-09-26 23:24:01 +0300

    Chenge access protected members length, head and getNode from class ListOnLinkedList
    https://stackoverflow.com/questions/6592512/templates-parent-class-member-variables-not-visible-in-inherited-class

--- Laboratory work 1/Laboratory work 1/CyclicListOnLinkedList.h
+++ Laboratory work 1/Laboratory work 1/CyclicListOnLinkedList.h
@@ -6,10 +6,9 @@
 template<typename T>
 class CyclicListOnLinckedList : public ListOnLinkedList<T>
 {
-	//Node<T> *head = nullptr;
-	//int length;
-
-	Node<T> *getNode(int position);
+	using ListOnLinkedList<T>::length;
+	using ListOnLinkedList<T>::head;
+	using ListOnLinkedList<T>::getNode;
 
 public:
 	void pushBack(T value);
@@ -19,48 +18,37 @@ public:
 	void reverse();
 };
 
-template<typename T>
-Node<T> *CyclicListOnLinckedList<T>::getNode(int position)
-{
-	Node<T> *node = this->head;
-	for (int i = 0; i < position; ++i)
-	{
-		node = node->next;
-	}
-	return node;
-}
-
 
 template<typename T>
 void CyclicListOnLinckedList<T>::pushBack(T value)
 {
 	Node<T>  *newNode = new Node<T>(value);
-	if (this->length == 0)
+	if (length == 0)
 	{
-		this->head = newNode;
-		this->head->next = this->head;
+		head = newNode;
+		head->next = head;
 	}
 	else
 	{
-		Node<T> *node = getNode(this->length - 1);
+		Node<T> *node = getNode(length - 1);
 
 		node->next = newNode;
-		newNode->next = this->head;
+		newNode->next = head;
 	}
-	++this->length;
+	++length;
 }
 
 template<typename T>
 void CyclicListOnLinckedList<T>::popBack()
 {
-	if (this->length != 0)
+	if (length != 0)
 	{
-		Node<T> *node = getNode(this->length - 2);
+		Node<T> *node = getNode(length - 2);
 		Node<T> *oldNode = node->next;
 
-		node->next = this->head;
+		node->next = head;
 		delete oldNode;
-		--this->length;
+		--length;
 	}
 }
 
@@ -68,46 +56,46 @@ template<typename T>
 void CyclicListOnLinckedList<T>::pushFront(T value)
 {
 	Node<T> *newNode = new Node<T>(value);
-	if (this->length == 0)
+	if (length == 0)
 	{
-		this->head = newNode;
-		this->head->next = this->head;
+		head = newNode;
+		head->next = head;
 	}
 	else
 	{
-		Node<T> *node = getNode(this->length - 1);
+		Node<T> *node = getNode(length - 1);
 		node->next = newNode;
-		newNode->next = this->head;
-		this->head = newNode;
+		newNode->next = head;
+		head = newNode;
 	}
-	++this->length;
+	++length;
 }
 
 template<typename T>
 void CyclicListOnLinckedList<T>::popFront()
 {
-	if (this->length != 0)
+	if (length != 0)
 	{
-		Node<T> *node = getNode(this->length - 1);
-		Node<T> *oldNode = this->head;
-		node->next = this->head->next;
-		this->head = this->head->next;
+		Node<T> *node = getNode(length - 1);
+		Node<T> *oldNode = head;
+		node->next = head->next;
+		head = head->next;
 		delete oldNode;
-		--this->length;
+		--length;
 	}
 }
 
 template<typename T>
 void CyclicListOnLinckedList<T>::reverse()
 {
-	Node<T> *nodeLast = this->head;
-	Node<T> *nodeNow = this->head;
-	Node<T> *nodeNext = this->head->next;
+	Node<T> *nodeLast = head;
+	Node<T> *nodeNow = head;
+	Node<T> *nodeNext = head->next;
 	nodeLast->next = nullptr;
 	Node<T> *tail = nodeLast;
 	nodeNow = nodeNext;
 	nodeNext = nodeNext->next;
-	for (int i = 1; i < this->length - 1; ++i)
+	for (int i = 1; i < length - 1; ++i)
 	{
 		nodeNow->next = nodeLast;
 		nodeLast = nodeNow;
@@ -115,6 +103,6 @@ void CyclicListOnLinckedList<T>::reverse()
 		nodeNext = nodeNext->next;
 	}
 	nodeNow->next = nodeLast;
-	this->head = nodeNow;
-	tail->next = this->head;
+	head = nodeNow;
+	tail->next = head;
 }
