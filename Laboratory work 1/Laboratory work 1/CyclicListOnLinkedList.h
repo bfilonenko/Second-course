6881f4cfbad7b6a5185ff57af6f506a010b9f7b7
NKM6795 <31067906+NKM6795@users.noreply.github.com>
2018-09-26 23:02:05 +0300

    Add Cyclic List on lincked list

--- Laboratory work 1/Laboratory work 1/CyclicListOnLinkedList.h
+++ Laboratory work 1/Laboratory work 1/CyclicListOnLinkedList.h
@@ -6,8 +6,8 @@
 template<typename T>
 class CyclicListOnLinckedList : public ListOnLinkedList<T>
 {
-	Node<T> *head = nullptr;
-	int length;
+	//Node<T> *head = nullptr;
+	//int length;
 
 	Node<T> *getNode(int position);
 
@@ -22,7 +22,7 @@ public:
 template<typename T>
 Node<T> *CyclicListOnLinckedList<T>::getNode(int position)
 {
-	Node<T> *node = head;
+	Node<T> *node = this->head;
 	for (int i = 0; i < position; ++i)
 	{
 		node = node->next;
@@ -35,32 +35,32 @@ template<typename T>
 void CyclicListOnLinckedList<T>::pushBack(T value)
 {
 	Node<T>  *newNode = new Node<T>(value);
-	if (length == 0)
+	if (this->length == 0)
 	{
-		head = newNode;
-		head->next = head;
+		this->head = newNode;
+		this->head->next = this->head;
 	}
 	else
 	{
-		Node<T> *node = getNode(length - 1);
+		Node<T> *node = getNode(this->length - 1);
 
 		node->next = newNode;
-		newNode->next = head;
+		newNode->next = this->head;
 	}
-	++length;
+	++this->length;
 }
 
 template<typename T>
 void CyclicListOnLinckedList<T>::popBack()
 {
-	if (length != 0)
+	if (this->length != 0)
 	{
-		Node<T> *node = getNode(length - 2);
+		Node<T> *node = getNode(this->length - 2);
 		Node<T> *oldNode = node->next;
 
-		node->next = head;
+		node->next = this->head;
 		delete oldNode;
-		--length;
+		--this->length;
 	}
 }
 
@@ -68,46 +68,46 @@ template<typename T>
 void CyclicListOnLinckedList<T>::pushFront(T value)
 {
 	Node<T> *newNode = new Node<T>(value);
-	if (length == 0)
+	if (this->length == 0)
 	{
-		head = newNode;
-		head->next = head;
+		this->head = newNode;
+		this->head->next = this->head;
 	}
 	else
 	{
-		Node<T> *node = getNode(length - 1);
+		Node<T> *node = getNode(this->length - 1);
 		node->next = newNode;
-		newNode->next = head;
-		head = newNode;
+		newNode->next = this->head;
+		this->head = newNode;
 	}
-	++length;
+	++this->length;
 }
 
 template<typename T>
 void CyclicListOnLinckedList<T>::popFront()
 {
-	if (length != 0)
+	if (this->length != 0)
 	{
-		Node<T> *node = getNode(length - 1);
-		Node<T> *oldNode = head;
-		node->next = head->next;
-		head = head->next;
+		Node<T> *node = getNode(this->length - 1);
+		Node<T> *oldNode = this->head;
+		node->next = this->head->next;
+		this->head = this->head->next;
 		delete oldNode;
-		--length;
+		--this->length;
 	}
 }
 
 template<typename T>
 void CyclicListOnLinckedList<T>::reverse()
 {
-	Node<T> *nodeLast = head;
-	Node<T> *nodeNow = head;
-	Node<T> *nodeNext = head->next;
+	Node<T> *nodeLast = this->head;
+	Node<T> *nodeNow = this->head;
+	Node<T> *nodeNext = this->head->next;
 	nodeLast->next = nullptr;
 	Node<T> *tail = nodeLast;
 	nodeNow = nodeNext;
 	nodeNext = nodeNext->next;
-	for (int i = 1; i < length - 1; ++i)
+	for (int i = 1; i < this->length - 1; ++i)
 	{
 		nodeNow->next = nodeLast;
 		nodeLast = nodeNow;
@@ -115,6 +115,6 @@ void CyclicListOnLinckedList<T>::reverse()
 		nodeNext = nodeNext->next;
 	}
 	nodeNow->next = nodeLast;
-	head = nodeNow;
-	tail->next = head;
+	this->head = nodeNow;
+	tail->next = this->head;
 }
