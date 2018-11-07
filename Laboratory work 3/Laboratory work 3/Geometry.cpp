c263933523ec58fa4e4fad48d7985f6d2ee9787e
NKM6795 <31067906+NKM6795@users.noreply.github.com>
2018-11-07 02:11:18 +0200

    Replaced Geometry

--- Laboratory work 3/Laboratory work 3/Geometry.cpp
+++ Laboratory work 3/Laboratory work 3/Geometry.cpp
@@ -3,229 +3,53 @@
 
 template<>
 template<>
-Vector3<int>::Vector3(const Vector3<float> &vector) : x(int(vector.x + 0.5f)), y(int(vector.y + 0.5f)), z(int(vector.z + 0.5f))
-{
-
-}
+Vector<3, int>::Vector(const Vector<3, float> &vector) : x(int(vector.x + 0.5f)), y(int(vector.y + 0.5f)), z(int(vector.z + 0.5f)) {}
 
 template<>
 template<>
-Vector3<float>::Vector3(const Vector3<int> &vector) : x(float(vector.x)), y(float(vector.y)), z(float(vector.z))
-{
-
-}
-
+Vector<3, float>::Vector(const Vector<3, int> &vector) : x(float(vector.x)), y(float(vector.y)), z(float(vector.z)) {}
 
 template<>
-Vector3<float>::Vector3(Matrix matrix) : x(matrix[0][0] / matrix[3][0]), y(matrix[1][0] / matrix[3][0]), z(matrix[2][0] / matrix[3][0])
-{
+template<>
+Vector<2, int>::Vector(const Vector<2, float> &vector) : x(int(vector.x + 0.5f)), y(int(vector.y + 0.5f)) {}
 
-}
-
-
-Matrix::Matrix(int r, int c) : matrix(vector<vector<float> >(r, vector<float>(c, 0.f))), rows(r), cols(c)
-{
-
-}
-
-int Matrix::getNumberOfRow()
-{
-	return rows;
-}
-
-int Matrix::getNumberOfCol()
-{
-	return cols;
-}
-
-Matrix Matrix::identity(int dimensions)
-{
-	Matrix E(dimensions, dimensions);
-
-	for (int i = 0; i < dimensions; ++i)
-	{
-		for (int j = 0; j < dimensions; ++j)
-		{
-			E[i][j] = (i == j ? 1.f : 0.f);
-		}
-	}
-
-	return E;
-}
-
-vector<float> &Matrix::operator [] (const int i)
-{
-	assert(i >= 0 && i < rows);
-
-	return matrix[i];
-}
-
-Matrix Matrix::operator * (const Matrix &right)
-{
-	assert(cols == right.rows);
-
-	Matrix result(rows, right.cols);
-
-	for (int i = 0; i < rows; ++i)
-	{
-		for (int j = 0; j < right.cols; ++j)
-		{
-			result.matrix[i][j] = 0.f;
-			for (int k = 0; k < cols; k++)
-			{
-				result.matrix[i][j] += matrix[i][k] * right.matrix[k][j];
-			}
-		}
-	}
-
-	return result;
-}
-
-Matrix Matrix::transpose()
-{
-	Matrix result(cols, rows);
-
-	for (int i = 0; i < rows; i++)
-	{
-		for (int j = 0; j < cols; j++)
-		{
-			result[j][i] = matrix[i][j];
-		}
-	}
-
-	return result;
-}
-
-Matrix Matrix::inverse()
-{
-	assert(rows == cols);
-
-	//Augmenting the square matrix with the identity matrix of the same dimensions a => [ai]
-
-	Matrix result(rows, cols * 2);
-
-	for (int i = 0; i < rows; ++i)
-	{
-		for (int j = 0; j < cols; ++i)
-		{
-			result[i][j] = matrix[i][j];
-		}
-	}
-
-	for (int i = 0; i < rows; ++i)
-	{
-		result[i][i + cols] = 1;
-	}
-
-	//First pass
-	for (int i = 0; i < rows - 1; ++i)
-	{
-		//Normalize the first row
-		for (int j = result.cols - 1; j >= 0; --j)
-		{
-			result[i][j] /= result[i][i];
-		}
-
-		for (int j = i + 1; j < rows; ++j)
-		{
-			float coefficient = result[j][i];
-			for (int k = 0; k < result.cols; ++k)
-			{
-				result[j][k] -= result[i][k] * coefficient;
-			}
-		}
-	}
-
-	//Normalize the last row
-	for (int j = result.cols - 1; j >= rows - 1; --j)
-	{
-		result[rows - 1][j] /= result[rows - 1][rows - 1];
-	}
-
-	//Second pass
-	for (int i = rows - 1; i > 0; --i)
-	{
-		for (int j = i - 1; j >= 0; --j)
-		{
-			float coefficient = result[j][i];
-			for (int k = 0; k < result.cols; ++k)
-			{
-				result[j][k] -= result[i][k] * coefficient;
-			}
-		}
-	}
-
-	//Cut the identity matrix back
-	Matrix truncate(rows, cols);
-
-	for (int i = 0; i < rows; ++i)
-	{
-		for (int j = 0; j < cols; ++j)
-		{
-			truncate[i][j] = result[i][j + cols];
-		}
-	}
-
-	return truncate;
-}
-
-ostream &operator << (ostream &os, Matrix &m)
-{
-	for (int i = 0; i < m.getNumberOfRow(); ++i)
-	{
-		for (int j = 0; j < m.getNumberOfCol(); ++j)
-		{
-			os << m[i][j];
-			if (j < m.getNumberOfCol() - 1)
-			{
-				os << "\t";
-			}
-		}
-		os << "\n";
-	}
-	return os;
-}
+template<>
+template<>
+Vector<2, float>::Vector(const Vector<2, int> &vector) : x(float(vector.x)), y(float(vector.y)) {}
 
 
-Matrix::Matrix(Vector3float vect) : matrix(vector<vector<float> >(4, vector<float>(1, 1.f))), rows(4), cols(1)
-{
-	matrix[0][0] = vect.x;
-	matrix[1][0] = vect.y;
-	matrix[2][0] = vect.z;
-}
-
-Matrix viewport(int x, int y, int width, int height, int depth)
-{
-	Matrix matrix = Matrix::identity(4);
-
-	matrix[0][3] = x + width * 0.5f;
-	matrix[1][3] = y + width * 0.5f;
-	matrix[2][3] = depth * 0.5f;
-
-	matrix[0][0] = width * 0.5f;
-	matrix[1][1] = width * 0.5f;
-	matrix[2][2] = depth * 0.5f;
-
-	return matrix;
-}
-
-Matrix lookat(Vector3float eye, Vector3float center, Vector3float up)
-{
-	Vector3float z = (eye - center).normalize();
-	Vector3float x = (up^z).normalize();
-	Vector3float y = (z^x).normalize();
-
-	Matrix Minv = Matrix::identity(4);
-	Matrix Tr = Matrix::identity(4);
-
-	for (int i = 0; i < 3; i++)
-	{
-		Minv[0][i] = x[i];
-		Minv[1][i] = y[i];
-		Minv[2][i] = z[i];
-
-		Tr[i][3] = -center[i];
-	}
-
-	return Minv * Tr;
-}
+//Matrix viewport(int x, int y, int width, int height, int depth)
+//{
+//	Matrix matrix = Matrix::identity(4);
+//
+//	matrix[0][3] = x + width * 0.5f;
+//	matrix[1][3] = y + width * 0.5f;
+//	matrix[2][3] = depth * 0.5f;
+//
+//	matrix[0][0] = width * 0.5f;
+//	matrix[1][1] = width * 0.5f;
+//	matrix[2][2] = depth * 0.5f;
+//
+//	return matrix;
+//}
+//
+//Matrix lookat(Vector3float eye, Vector3float center, Vector3float up)
+//{
+//	Vector3float z = (eye - center).normalize();
+//	Vector3float x = (up^z).normalize();
+//	Vector3float y = (z^x).normalize();
+//
+//	Matrix Minv = Matrix::identity(4);
+//	Matrix Tr = Matrix::identity(4);
+//
+//	for (int i = 0; i < 3; i++)
+//	{
+//		Minv[0][i] = x[i];
+//		Minv[1][i] = y[i];
+//		Minv[2][i] = z[i];
+//
+//		Tr[i][3] = -center[i];
+//	}
+//
+//	return Minv * Tr;
+//}
--- Laboratory work 3/Laboratory work 3/Geometry.h
+++ Laboratory work 3/Laboratory work 3/Geometry.h
@@ -10,209 +10,372 @@
 using namespace std;
 
 
-template<typename T>
-struct Vector2
+template<size_t DimCols, size_t DimRows, typename T>
+class Matrix;
+
+
+
+template <size_t Dim, typename T>
+struct Vector
 {
-	T x, y;
+private:
+	T data[Dim];
 
-	Vector2<T>() : x(T()), y(T()) {}
-
-	Vector2<T>(T x, T y) : x(x), y(y) {}
-
-	Vector2<T>(const Vector2<T> &vector) : x(T()), y(T())
+public:
+	Vector()
 	{
-		*this = vector;
+		for (size_t i = Dim; i--; data[i] = T());
 	}
 
-	Vector2<T> & operator = (const Vector2 &right)
+	T &operator [] (const size_t i)
 	{
-		if (this != &right)
-		{
-			x = right.x;
-			y = right.y;
-		}
-		return *this;
+		assert(i < Dim);
+		
+		return data[i];
 	}
 
-	inline Vector2<T> operator + (const Vector2<T> &right) const
+	const T &operator [] (const size_t i) const
 	{
-		return Vector2<T>(x + right.x, y + right.y);
-	}
-
-	inline Vector2<T> operator - (const Vector2<T> &right) const
-	{
-		return Vector2<T>(x - right.x, y - right.y);
-	}
-
-	inline Vector2<T> operator * (float value) const
-	{
-		return Vector2<T>(T (x * value), T (y * value));
-	}
-
-	friend ostream &operator << (ostream &os, Vector2<T> &vector);
-
-	T &operator [] (const int i)
-	{
-		if (i <= 0)
-		{
-			return x;
-		}
-		else
-		{
-			return y;
-		}
+		assert(i < Dim);
+		return data[i];
 	}
 };
 
 
+
 template <typename T>
-ostream &operator << (ostream &os, Vector2<T> &vector)
+struct Vector<2, T>
 {
-	os << "(" << vector.x << ", " << vector.y << ")\n";
-	return os;
-}
+	T x, y;
 
+	Vector() : x(T()), y(T())
+	{
 
-class Matrix;
+	}
 
+	Vector(T x, T y) : x(x), y(y)
+	{
 
-template<typename T>
-struct Vector3
+	}
+
+	template <class U>
+	Vector<2, T>(const Vector<2, U> &vector);
+
+	T &operator [] (const size_t i)
+	{
+		assert(i < 2);
+		
+		return i <= 0 ? x : y;
+	}
+
+	const T &operator [] (const size_t i) const 
+	{
+		assert(i < 2);
+		
+		return i <= 0 ? x : y;
+	}
+};
+
+template <typename T>
+struct Vector<3, T>
 {
 	T x, y, z;
 
-	Vector3<T>() : x(T()), y(T()), z(T()) {}
-
-	Vector3<T>(T x, T y, T z) : x(x), y(y), z(z) {}
-
-	Vector3<T>(Matrix matrix);
-
-	template<typename U>
-	Vector3<T>(const Vector3<U> &right);
-
-	Vector3<T>(const Vector3 &vector) : x(T()), y(T()), z(T())
+	Vector() : x(T()), y(T()), z(T())
 	{
-		*this = vector;
+
 	}
 
-	Vector3<T> & operator = (const Vector3<T> &right)
+	Vector(T x, T y, T z) : x(x), y(y), z(z)
 	{
-		if (this != &right)
-		{
-			x = right.x;
-			y = right.y;
-			z = right.z;
-		}
-		return *this;
+
 	}
 
-	inline Vector3<T> operator ^ (const Vector3<T> &right) const
+	template <class U>
+	Vector<3, T>(const Vector<3, U> &vector);
+
+	T &operator [] (const size_t i)
 	{
-		return Vector3<T>(y * right.z - z * right.y, z * right.x - x * right.z, x * right.y - y * right.x);
+		assert(i < 3);
+		
+		return i <= 0 ? x : (1 == i ? y : z);
 	}
 
-	inline Vector3<T> operator + (const Vector3<T> &right) const
+	const T &operator [] (const size_t i) const
 	{
-		return Vector3<T>(x + right.x, y + right.y, z + right.z);
+		assert(i < 3);
+		
+		return i <= 0 ? x : (1 == i ? y : z);
 	}
 
-	inline Vector3<T> operator - (const Vector3<T> &right) const
-	{
-		return Vector3<T>(x - right.x, y - right.y, z - right.z);
-	}
-
-	inline Vector3<T> operator * (float value) const
-	{
-		return Vector3<T>(x * value, y * value, z * value);
-	}
-
-	inline T operator * (const Vector3<T> &right) const
-	{
-		return x * right.x + y * right.y + z * right.z;
-	}
-
-	float norm() const
+	float norm()
 	{
 		return sqrt(x * x + y * y + z * z);
 	}
 
-	Vector3<T> normalize(T l = 1)
+	Vector<3, T> &normalize(T l = 1)
 	{
-		*this = (*this) * (l / norm());
+		*this = (*this)*(l / norm());
+		
 		return *this;
-	}
-
-	friend ostream &operator << (ostream &os, Vector3<T> &vector);
-
-	T &operator [] (const int i)
-	{
-		if (i <= 0)
-		{
-			return x;
-		}
-		else if (i == 1)
-		{
-			return y;
-		}
-		else
-		{
-			return z;
-		}
 	}
 };
 
 
-template <typename T>
-ostream &operator << (ostream &os, Vector3<T> &vector)
+
+template<size_t Dim, typename T>
+T operator * (const Vector<Dim, T> &left, const Vector<Dim, T> &right)
 {
-	os << "(" << vector.x << ", " << vector.y << ", " << vector.z << ")\n";
+	T result = T();
+
+	for (size_t i = Dim; i--; result += left[i] * right[i]);
+	
+	return result;
+}
+
+template<size_t Dim, typename T>
+Vector<Dim, T> operator + (Vector<Dim, T> &left, const Vector<Dim, T> &right)
+{
+	for (size_t i = Dim; i--; left[i] += right[i]);
+
+	return left;
+}
+
+template<size_t Dim, typename T>
+Vector<Dim, T> operator - (Vector<Dim, T> &left, const Vector<Dim, T> &right)
+{
+	for (size_t i = Dim; i--; left[i] -= right[i]);
+
+	return left;
+}
+
+template<size_t Dim, typename T, typename U>
+Vector<Dim, T> operator * (Vector<Dim, T> &left, const U &right)
+{
+	for (size_t i = Dim; i--; left[i] *= right);
+
+	return left;
+}
+
+template<size_t Dim, typename T, typename U>
+Vector<Dim, T> operator / (Vector<Dim, T> &left, const U &right)
+{
+	for (size_t i = Dim; i--; left[i] /= right);
+
+	return left;
+}
+
+template<size_t Len, size_t Dim, typename T>
+Vector<Len, T> embed(const Vector<Dim, T> &v, T fill = 1)
+{
+	Vector<Len, T> result;
+
+	for (size_t i = Len; i--; result[i] = (i < Dim ? v[i] : fill));
+
+	return result;
+}
+
+template<size_t Len, size_t Dim, typename T>
+Vector<Len, T> proj(const Vector<Dim, T> &vector)
+{
+	Vector<Len, T> result;
+
+	for (size_t i = Len; i--; result[i] = vector[i]);
+
+	return result;
+}
+
+template <typename T>
+Vector<3, T> cross(Vector<3, T> vector1, Vector<3, T> vector2)
+{
+	return Vector<3, T>(vector1.y * vector2.z - vector1.z * vector2.y, vector1.z * vector2.x - vector1.x * vector2.z, vector1.x * vector2.y - vector1.y * vector2.x);
+}
+
+template <size_t Dim, typename T>
+ostream &operator << (ostream &os, Vector<Dim, T> &vector)
+{
+	for (unsigned int i = 0; i < Dim; ++i)
+	{
+		os << vector[i] << " ";
+	}
 	return os;
 }
 
 
-typedef Vector2<float> Vector2float;
-typedef Vector2<int>   Vector2int;
-typedef Vector3<float> Vector3float;
-typedef Vector3<int>   Vector3int;
 
-
-template<>
-template<>
-Vector3<int>::Vector3(const Vector3<float> &vector);
-
-template<>
-template<>
-Vector3<float>::Vector3(const Vector3<int> &vector);
-
-
-class Matrix
+template<size_t Dim, typename T>
+struct dt
 {
-	vector<vector<float> > matrix;
+	static T determine(const Matrix<Dim, Dim, T> &src)
+	{
+		T result = 0;
 
-	int rows, cols;
+		for (size_t i = Dim; i--; result += src[0][i] * src.cofactor(0, i));
 
-public:
-	Matrix(int r = 4, int c = 4);
+		return result;
+	}
+};
 
-	Matrix(Vector3float vector);
-
-	inline int getNumberOfRow();
-	inline int getNumberOfCol();
-
-	static Matrix identity(int dimensions);
-
-	vector<float> &operator [] (const int i);
-
-	Matrix operator*(const Matrix& right);
-
-	Matrix transpose();
-
-	Matrix inverse();
-
-	friend ostream &operator << (ostream &os, Matrix &m);
+template<typename T>
+struct dt<1, T>
+{
+	static T determine(const Matrix<1, 1, T> &src)
+	{
+		return src[0][0];
+	}
 };
 
 
-Matrix viewport(int x, int y, int width, int height, int depth);
 
-Matrix lookat(Vector3float eye, Vector3float center, Vector3float up);
+template<size_t DimRows, size_t DimCols, typename T>
+class Matrix
+{
+	Vector<DimCols, T> rows[DimRows];
+
+public:
+	Matrix() {}
+
+	Vector<DimCols, T> &operator [] (const size_t i)
+	{
+		assert(i < DimRows);
+
+		return rows[i];
+	}
+
+	const Vector<DimCols, T> &operator [] (const size_t i) const
+	{
+		assert(i < DimRows);
+
+		return rows[i];
+	}
+
+	Vector<DimRows, T> col(const size_t i) const
+	{
+		assert(i < DimCols);
+
+		Vector<DimRows, T> result;
+		
+		for (size_t j = DimRows; j--; result[j] = rows[j][i]);
+		
+		return result;
+	}
+
+	void setCol(size_t i, Vector<DimRows, T> vector)
+	{
+		assert(i < DimCols);
+
+		for (size_t j = DimRows; j--; rows[j][i] = vector[j]);
+	}
+
+	static Matrix<DimRows, DimCols, T> identity()
+	{
+		Matrix<DimRows, DimCols, T> result;
+
+		for (size_t i = DimRows; i--;)
+		{
+			for (size_t j = DimCols; j--; result[i][j] = (i == j));
+		}
+		
+		return result;
+	}
+
+	T determine() const
+	{
+		return dt<DimCols, T>::determine(*this);
+	}
+
+	Matrix<DimRows - 1, DimCols - 1, T> getMinor(size_t row, size_t col) const
+	{
+		Matrix<DimRows - 1, DimCols - 1, T> result;
+
+		for (size_t i = DimRows - 1; i--; )
+		{
+			for (size_t j = DimCols - 1; j--; result[i][j] = rows[i < row ? i : i + 1][j < col ? j : j + 1]);
+		}
+
+		return result;
+	}
+
+	T cofactor(size_t row, size_t col) const
+	{
+		return getMinor(row, col).determine()*((row + col) % 2 ? -1 : 1);
+	}
+
+	Matrix<DimRows, DimCols, T> adjugate() const
+	{
+		Matrix<DimRows, DimCols, T> result;
+
+		for (size_t i = DimRows; i--; )
+		{
+			for (size_t j = DimCols; j--; result[i][j] = cofactor(i, j));
+		}
+
+		return result;
+	}
+
+	Matrix<DimRows, DimCols, T> invertTranspose()
+	{
+		Matrix<DimRows, DimCols, T> result = adjugate();
+
+		T temp = result[0] * rows[0];
+
+		return result / temp;
+	}
+};
+
+
+
+template<size_t DimRows, size_t DimCols, typename T>
+Vector<DimRows, T> operator * (const Matrix<DimRows, DimCols, T> &left, const Vector<DimCols, T> &right)
+{
+	Vector<DimRows, T> result;
+
+	for (size_t i = DimRows; i--; result[i] = left[i] * right);
+
+	return result;
+}
+
+template<size_t DimRows1, size_t DimCols1, size_t DimCols2, typename T>
+Matrix<DimRows1, DimCols2, T> operator * (const Matrix<DimRows1, DimCols1, T> &left, const Matrix<DimCols1, DimCols2, T> &right)
+{
+	Matrix<DimRows1, DimCols2, T> result;
+
+	for (size_t i = DimRows1; i--; )
+	{
+		for (size_t j = DimCols2; j--; result[i][j] = left[i] * right.col(j));
+	}
+
+	return result;
+}
+
+template<size_t DimRows, size_t DimCols, typename T>
+Matrix<DimCols, DimRows, T> operator / (Matrix<DimRows, DimCols, T> left, const T &right)
+{
+	for (size_t i = DimRows; i--; left[i] = left[i] / right);
+
+	return left;
+}
+
+template <size_t DimRows, size_t DimCols, typename T>
+ostream &operator << (ostream& os, Matrix<DimRows, DimCols, T> &matrix)
+{
+	for (size_t i = 0; i < DimRows; ++i)
+	{
+		os << matrix[i] << '\n';
+	}
+
+	return os;
+}
+
+
+
+typedef Vector<2, float> Vector2float;
+typedef Vector<2, int>   Vector2int;
+typedef Vector<3, float> Vector3float;
+typedef Vector<3, int>   Vector3int;
+typedef Vector<4, float> Vector4float;
+typedef Matrix<4, 4, float> Matrix4float;
+
+//Matrix viewport(int x, int y, int width, int height, int depth);
+//
+//Matrix lookat(Vector3float eye, Vector3float center, Vector3float up);
